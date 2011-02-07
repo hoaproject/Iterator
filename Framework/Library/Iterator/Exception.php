74d61291bea631a3ea9791cc04c8074ca2871547
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2011-02-07 11:59:33 +0100

    Change Hoa_Exception for Hoa_Core_Exception.

--- Framework/Library/Iterator/Exception.php
+++ Framework/Library/Iterator/Exception.php
@@ -35,7 +35,7 @@
 /**
  * Class Hoa_Iterator_Exception.
  *
- * Extending the Hoa_Exception class.
+ * Extending the Hoa_Core_Exception class.
  *
  * @author      Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
  * @copyright   Copyright (c) 2007, 2010 Ivan ENDERLIN.
@@ -46,4 +46,4 @@
  * @subpackage  Hoa_Iterator_Exception
  */
 
-class Hoa_Iterator_Exception extends Hoa_Exception { }
+class Hoa_Iterator_Exception extends Hoa_Core_Exception { }
