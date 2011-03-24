209fe02b2b7a9e0d17503a0d0ca66731e023efb1
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2011-03-24 12:06:30 +0100

    Change ENDERLIN to Enderlin '-.-.

--- Framework/Library/Iterator/Basic.php
+++ Framework/Library/Iterator/Basic.php
@@ -62,8 +62,8 @@ namespace Hoa\Iterator {
  *
  * Make a simple and quick iterator of any types.
  *
- * @author     Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
- * @copyright  Copyright © 2007-2011 Ivan ENDERLIN.
+ * @author     Ivan Enderlin <ivan.enderlin@hoa-project.net>
+ * @copyright  Copyright © 2007-2011 Ivan Enderlin.
  * @license    New BSD License
  */
 
--- Framework/Library/Iterator/Recursive.php
+++ Framework/Library/Iterator/Recursive.php
@@ -41,8 +41,8 @@ namespace Hoa\Iterator {
  *
  * Extending the SPL RecursiveIterator interface.
  *
- * @author     Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
- * @copyright  Copyright © 2007-2011 Ivan ENDERLIN.
+ * @author     Ivan Enderlin <ivan.enderlin@hoa-project.net>
+ * @copyright  Copyright © 2007-2011 Ivan Enderlin.
  * @license    New BSD License
  */
 
