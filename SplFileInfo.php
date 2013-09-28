db89bfa9fd46f8fa14c8258e1484f12dc3d4a398
Derver <matt-2408@hotmail.fr>
2013-09-28 15:23:45 +0200

    Change ctime for mtime when computing the hash.
    Original commit message below.
    
    Add modify time on Windows
    
    on Windows:
    getCTime() return the file's creation time
    getMTime() return the last modified time
    
    Used by \Hoa\File\Watcher
    
    Bug from Win OS or NTFS file format ?

--- SplFileInfo.php
+++ SplFileInfo.php
@@ -78,7 +78,7 @@ class SplFileInfo extends \SplFileInfo {
 
         $this->_hash = md5(
             $this->getPathname() .
-            $this->getCTime()
+            (OS_WIN) ? $this->getMTime() : $this->getCTime()
         );
         $this->_relativePath = $relativePath;
 
